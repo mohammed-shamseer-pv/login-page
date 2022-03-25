0555f099c7e0fd815f4b0f6619331cbc87eafacc
shamseer <mohammedshamseerp@gmail.com>
2022-03-25 14:43:56 -0700

    form validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,30 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+
+
+
+function validate() {
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+
+  if (username.value == "") {
+    alert("plese enter your youser username");
+    document.form.username.focuss();
+    return false;
+  }
+  if (email.value == "") {
+    alert("plese enter your youser email");
+    document.form.email.focuss();
+    return false;
+  }
+
+  if (password.value == "") {
+    alert("plese enter your youser password");
+    document.form.password.focuss();
+    return false;
+  }
+
+  return true;
+}
